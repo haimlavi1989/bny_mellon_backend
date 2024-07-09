9aac6c0081c76e920cbf3707fb3644797e5a3016
haimlavi1989 <haimlavi1989@gmail.com>
2024-07-09 20:26:20 +0300

    Init project template

---
+++ routes/userRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const Router = express.Router();
+const authController = require('./../controllers/authController');
+const signupValidation = require('../validation/signupValidation');
+const validationMiddleware = require('../validation/validationMiddleware');
+
+Router.post('/signup',
+  signupValidation.signupValidationRules(),
+  validationMiddleware.validateRequest,
+  authController.signup);
+Router.post('/login',
+  authController.login
+);
+
+Router.post('/forgotPassword', authController.forgotPassword);
+Router.patch('/resetPassword/:token', authController.resetPassword);
+
+/*
+Router.route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+*/
+Router.route('/:id');
+  //.get(userController.getUser)
+  //.delete(userController.deleteUser);
+
+// Router.route('/updateUser')
+// .patch(userController.updateUser);
+
+
+module.exports = Router;
